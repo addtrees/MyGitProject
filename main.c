cc340072b9490f5b819779410c16ab14bbf5caa6
shencr026 <shencr0929@gmail.com>
2022-10-05 12:59:14 +0300

    danil add some change
    # Conflicts:
    #	main.c

--- main.c
+++ main.c
@@ -10,5 +10,9 @@ int main(int agrc,char **agrv)
     printf("Hello %s %s!\n", first, last);
     return 0;
     // add my little change
+<<<<<<< HEAD
     // ok, I saw  your change
+=======
+    // danil's change
+>>>>>>> e03d7f3 (danil add some change)
 }
